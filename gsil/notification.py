0b94f1cee44261991bbd9c4a47a257b8810da9e8
Anye <Anye@users.noreply.github.com>
2018-05-29 19:28:29 +0800

    支持 465 ssl 接口

--- gsil/notification.py
+++ gsil/notification.py
@@ -53,9 +53,12 @@ class Notification(object):
         msg.attach(text)
 
         try:
-            s = smtplib.SMTP(get('mail', 'host'), get('mail', 'port'))
-            s.ehlo()
-            s.starttls()
+            if port == '465':
+                s = smtplib.SMTP_SSL(host, port)
+            else:
+                s = smtplib.SMTP(host, port)
+                s.ehlo()
+                s.starttls()
             s.ehlo()
             s.login(mail, get('mail', 'password'))
             s.sendmail(mail, self.to.split(','), msg.as_string())
